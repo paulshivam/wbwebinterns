8a72bd22e62c3c15aa63ebac7d507ad1163a65c2
Sarthak-Rout <sarthakr97@gmail.com>
2019-06-06 02:55:04 +0530

    login page created

---
+++ sarthak-rout/index.php
@@ -0,0 +1,52 @@
+<?php
+  include '../config/db.php';
+  include '../config/session.php';
+  include '../lib/functions.php';
+  if (isset($_POST['username'])){
+    $username=$_POST['username'];
+    $pwd=$_POST['password'];
+    $found=0;
+    if($query=mysqli_query("SELECT * FROM users WHERE username='$username' AND password='$pwd'"))
+      while($row=$mysqli_fetch_array($query))
+        $found = 1;
+
+    if ($found == 1){
+      echo "Login Successful";
+    }
+    else{
+      echo "Login Unsuccessful";
+    }
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>qwerty</title>
+  </head>
+  <style>
+    .login{
+      border: 2px solid blue;
+      width: 300px;
+      height: 150px;
+      padding-top: 20px;
+      padding-left: 20px;
+      background: #398;
+      margin:auto;s
+
+    }
+
+  </style>
+  <body>
+    <div class="login">
+      <form method="post">
+        USERNAME: <input type="text" name="username" placeholder="Enter username"><br><br>
+        PASSWORD: <input type="password" name="password" placeholder="Enter password"><br><br>
+        <center><button type="submit" name="button" style="background: white;">Log In</button></center>
+      </form>
+    </div>
+
+
+  </body>
+</html>
