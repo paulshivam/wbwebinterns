d04b943a42c086623f19053e226f21473e499e5b
Biswabijaya Samal <me@biswabijaya.com>
2019-06-06 01:00:40 +0530

    Week 1
    basic configuration

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+//initialise database connection variables
+$databaseHost = 'localhost';
+$databaseName = 'u660698074_db';
+$databaseUsername = 'u660698074_user';
+$databasePassword = 'db@user@123';
+
+//create connection
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+
+?>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+  include 'config/db.php';
+  include 'config/session.php';
+  include 'lib/functions.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
