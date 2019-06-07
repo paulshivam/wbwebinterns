f254936c375a73cb5681affcdc8e0aea94032739
SHIVAM <me.shivampaul@gmail.com>
2019-06-08 02:25:02 +0530

    basic CRUD added

---
+++ paulshivam/displayall.php
@@ -0,0 +1,53 @@
+<?php
+
+//packages to include
+  include '../config/db.php';
+  include '../config/session.php';
+  include '../lib/functions.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Display</title>
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  </head>
+  <body>
+
+      <table border="1">
+        <thead>
+          <th>No</th>
+          <th>Username</th>
+          <th>Password</th>
+          <th>Action</th>7
+        </thead>
+        <tbody>
+
+            <?php
+
+              if ($query=mysqli_query($mysqli,"SELECT * FROM users")) {
+                  while ($row=mysqli_fetch_array($query)) {
+                    echo "<tr>";
+                    echo '<td>'.$row['id'].'</td>';
+                    echo '<td>'.$row['username'].'</td>';
+                    echo '<td>'.$row['password'].'</td>';
+                    echo '<td align="center"><i class="fa fa-pencil-square" aria-hidden="true" style="color:orange"></i>&nbsp;<i class="fa fa-times" aria-hidden="true" style="color:red"></i></td>';
+                    echo "</tr>";
+
+                  }
+              }
+             ?>
+        </tbody>
+        <tfoot>
+          <th>No</th>
+          <th>Username</th>
+          <th>Password</th>
+          <th>Action</th>7
+        </tfoot>
+
+      </table>
+  </body>
+
+</html>
---
+++ paulshivam/index.php
@@ -0,0 +1,36 @@
+<?php
+  include '../config/db.php';
+  include '../config/session.php';
+  include '../lib/functions.php';
+
+  if (isset($_GET['msg'])) {
+    $msg=$_GET['msg'];
+  } else {
+    $msg='';
+  }
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome to WB WEB Code Lab</title>
+  </head>
+  <body>
+    <h1><?php echo $msg; ?></h1>
+    <?php if(isset($_SESSION['id'])){ ?>
+        <br>
+        <h1>Welcome <?php echo $_SESSION['username']; ?></h1>
+        <button type="button" name="button" onclick="location.href='logout.php'">Logout</button>
+      <?php } else { ?>
+
+        <button type="button" name="button" onclick="location.href='login.php'">Login</button>
+        <button type="button" name="button" onclick="location.href='register.php'">Register</button>
+      <?php } ?>
+
+        <button type="button" name="button" onclick="location.href='displayall.php'">Display All Records</button>
+
+  </body>
+</html>
