05496f7a81cf779dab355b8a3e371ce770c2d825
Biswabijaya Samal <me@biswabijaya.com>
2019-06-08 17:04:03 +0530

    update cdn

--- config/jscdn.php
+++ config/jscdn.php
@@ -1,4 +1,5 @@
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" ></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" ></script>
 <script src="https://code.jquery.com/jquery-3.4.1.js"> </script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
 <script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
