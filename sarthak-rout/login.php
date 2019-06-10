d8e28b8bd0923ce8c9412f690db983491e9f5522
Sarthak-Rout <sarthakr97@gmail.com>
2019-06-11 03:39:55 +0530

    css added

--- sarthak-rout/login.php
+++ sarthak-rout/login.php
@@ -45,22 +45,41 @@
     .login{
       border: 2px solid blue;
       width: 300px;
-      height: 150px;
+      height: 400px;
       padding-top: 20px;
       padding-left: 20px;
-      background: #398;
-      margin:auto;
+      margin: auto;
+      border: 1px solid #ccc;
+      box-sizing: border-box;
+      margin-top: 100px;
+      background-image: linear-gradient(-225deg, #FFE29F 0%, #FFA99F 48%, #FF719A 100%);
 
+    }
+
+    #logo{
+      width:100px;
+      height:100px;
+      margin:0px 70px 10px 70px;
+    }
+
+    input[type=text], input[type=password]{
+      width: 90%;
+    }
+
+    body{
+      background-image: linear-gradient(-225deg, #E3FDF5 0%, #FFE6FA 100%);
     }
 
   </style>
   <body>
     <div class="login">
       <form method="post">
-        USERNAME: <input type="text" name="username"  <?php  if ($iderror) echo 'placeholder="'.$iderror.'"  autofocus'; else if(isset($_POST['username'])) echo 'value="'.$_POST['username'].'"'; else echo 'placeholder="Enter username" autofocus'; ?> required><br><br>
-        PASSWORD: <input type="password" name="password"  <?php if($passerror) echo 'placeholder="'.$passerror.'" autofocus'; else echo 'placeholder="Enter password"' ;  ?> required><br><br>
-        <center><button type="submit" name="submit" value="login" style="background: white;">Log In</button></center>
-        <button type="button" name="button" onclick="javascript: if(confirm('Are you sure')) location.href='index.php'; ">Return</button>
+        <img src="https://d1p9wirkq0k00v.cloudfront.net/wp-content/uploads/2019/05/02084053/hr.png" id="logo"><br><br>
+        <b>USERNAME:</b> <input type="text" name="username"  <?php  if ($iderror) echo 'placeholder="'.$iderror.'"  autofocus'; else if(isset($_POST['username'])) echo 'value="'.$_POST['username'].'"'; else echo 'placeholder="Enter username" autofocus'; ?> required><br><br>
+        <b>PASSWORD:</b> <input type="password" name="password"  <?php if($passerror) echo 'placeholder="'.$passerror.'" autofocus'; else echo 'placeholder="Enter password"' ;  ?> required><br><br>
+        <button type="button" class="btn btn-light "name="button" onclick="javascript: if(confirm('Are you sure')) location.href='index.php'; ">Return</button>
+        <button type="submit" class="btn btn-danger" name="submit" value="login" style="margin-left:100px;">Log In</button>
+
 
       </form>
     </div>
--- sarthak-rout/register.php
+++ sarthak-rout/register.php
@@ -12,13 +12,11 @@ if (isset($_SESSION['id'])) {
 //global data member
   $msg = 0;
 
-
 //to handle post request for user register
   if (isset($_POST['submit']) and $_POST['submit']=='register'){
     $username=$_POST['username'];
     $password=$_POST['password'];
     $password=password_hash($password,PASSWORD_DEFAULT);
-
 
     $found=0;
     if ($query = mysqli_query($mysqli,"SELECT * from users WHERE username = '$username'")) {
@@ -49,24 +47,40 @@ if (isset($_SESSION['id'])) {
   </head>
   <style>
     .login{
-      border: 2px solid blue;
       width: 300px;
-      height: 150px;
+      height: 400px;
       padding-top: 20px;
       padding-left: 20px;
-      background: #398;
-      margin:auto;s
+      margin:auto;
+      border: 1px solid #ccc;
+      box-sizing: border-box;
+      margin-top: 100px;
+      background-image: linear-gradient(to top, rgb(48, 207, 208) 0%, rgb(51, 8, 103) 100%);
+    }
 
+    #logo{
+      width:100px;
+      height:100px;
+      margin:0px 70px 10px 70px;
+    }
+
+    input[type=text], input[type=password]{
+      width: 90%;
+    }
+    body{
+      background-image: linear-gradient(-225deg, #E3FDF5 0%, #FFE6FA 100%);
     }
 
   </style>
   <body>
     <div class="login">
       <form method="post">
-        USERNAME: <input type="text" name="username" placeholder="Enter username"><br><br>
-        PASSWORD: <input type="password" name="password" placeholder="Enter password"><br><br>
-        <center><button type="submit" name="submit" value="register" style="background: white;">Register</button></center>
-        <button type="button" name="button" onclick="javascript: if(confirm('Are you sure')) location.href='index.php'; ">Return</button>
+        <img src="https://image.flaticon.com/icons/png/512/306/306473.png" id="logo"><br><br>
+        <b>USERNAME:</b> <input type="text" name="username" placeholder="Enter username"><br><br>
+        <b>PASSWORD:</b> <input type="password" name="password" placeholder="Enter password"><br><br>
+        <button type="button" class="btn btn-light "name="button" onclick="javascript: if(confirm('Are you sure')) location.href='index.php'; ">Return</button>
+        <button type="submit" class="btn btn-success " name="submit" value="register" style="margin-left:80px;">Register</button>
+
       </form>
     </div>
     <?php include '../config/jscdn.php'; ?>
